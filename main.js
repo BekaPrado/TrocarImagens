95696291264899028d7255e15397073b6ebcb6aa
Beka Prado <rebeka.marcelino.pradogmail.com>
2025-02-03 22:41:48 -0300

    ajuste imagens

--- main.js
+++ main.js
@@ -3,10 +3,10 @@
 const botaoTrocarImagem = document.getElementById('trocar-imagem');
 
 const imagens = [
-    'url("imagens/salmo.jpg")',
-    'url("imagens/isaias.jpg")',
-    'url("imagens/mateus.jpg")',
-    'url("imagens/proverbios.jpg")'
+    'url("imagens/paratodososgarotos.jpg")',
+    'url("imagens/comoperder.jpg")',
+    'url("imagens/gossipgirl.jpg")',
+    'url("imagens/gilmore.jpg")'
 ];
 
 let indiceAtual = 0;
